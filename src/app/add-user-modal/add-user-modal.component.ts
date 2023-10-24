ffd54735608777416d9387983a81d111ce2066d4
Alan Daniel Alvarez Castro <alandanielalvarez0000@gmail.com>
2023-10-24 16:04:31 -0600

    feat: improved add user modal

--- src/app/add-user-modal/add-user-modal.component.ts
+++ src/app/add-user-modal/add-user-modal.component.ts
@@ -10,7 +10,8 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 export class AddUserModalComponent {
   newUser: any = {};
   companyId: string; // Propiedad para almacenar companyID
-  userType: string = 'Leader' || 'Employee'; // O 'Employee', según corresponda
+  departmentId: string; // Propiedad para almacenar companyID
+  type: boolean;
 
   constructor(
     private http: HttpClient,
@@ -18,39 +19,65 @@ export class AddUserModalComponent {
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {
     this.companyId = data.companyId; // Obtén companyID de las propiedades
-    this.userType = data.type; // Obtén el tipo de usuario de las propiedades
+    this.departmentId = data.departmentId; // Obtén companyID de las propiedades
+    this.type = data.type; // Obtén el tipo de usuario de las propiedades
   }
 
   addUser(): void {
-    const endpoint = `http://localhost:8080/api/departments/${this.companyId}`;
-  
-    const userToAdd: { [key: string]: any } = {
-      employees: null,
-      company: this.companyId,
-      leader: null
-    };
-  
-    if (this.userType === 'Leader') {
-      userToAdd['is_current_leader'] = true;
-      userToAdd['leader'] = true;
-    } else if (this.userType === 'Employee') {
-      userToAdd['is_current_leader'] = false;
-    }
+    console.log('company ID', this.companyId);
+    console.log('department ID', this.departmentId);
+    console.log('Lider?', this.type)
 
-    if (this.userType === 'Leader') {
-    } else if (this.userType === 'Employee') {
-    }
-  
-    this.http.put(endpoint, userToAdd).subscribe(
+    const employeesEndpoint = 'http://localhost:8080/api/employees'; // Endpoint para agregar un empleado
+    const departmentsEndpoint = `http://localhost:8080/api/departments/${this.departmentId}`;
+
+
+    const userToAdd: any = {
+      name: this.newUser.username,
+      department: this.departmentId,
+      company: this.companyId,
+      is_current_leader: this.type,// Verifica el tipo para establecer is_current_leader
+    };
+    
+    this.http.post(employeesEndpoint, userToAdd).subscribe(
       (data: any) => {
         console.log('User added successfully', data);
-        this.dialogRef.close(data);
+        console.log('ID de User', data.employeeId);
+        const departmentsEndpoint = `http://localhost:8080/api/departments/${this.departmentId}`;
+        
+        // Obtener la lista actual de empleados del departamento
+        this.http.get(departmentsEndpoint).subscribe(
+          (departmentData: any) => {
+            const currentEmployees = departmentData.employees || [];
+            
+            // Agregar el nuevo empleado a la lista actual
+            currentEmployees.push(data.employeeId);
+            const putData = this.type ? { leader: data.employeeId } : { employees: currentEmployees };
+
+            // Realizar una solicitud PUT para actualizar la lista de empleados en el departamento
+            this.http.put(departmentsEndpoint, putData).subscribe(
+              (updatedDepartmentData: any) => {
+                console.log('DEPARTMENT updated successfully', updatedDepartmentData);
+                this.dialogRef.close(data);
+                window.location.reload();
+              },
+              (departmentError: HttpErrorResponse) => {
+                console.error('Error updating Department', departmentError);
+                // Maneja los errores aquí
+              }
+            );
+          },
+          (departmentError: HttpErrorResponse) => {
+            console.error('Error fetching Department', departmentError);
+            // Maneja los errores aquí
+          }
+        );
       },
       (error: HttpErrorResponse) => {
         console.error('Error adding User', error);
         // Maneja los errores aquí
       }
     );
-  }
-  
+    
+}
 }
